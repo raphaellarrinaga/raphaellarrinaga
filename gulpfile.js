6bea3f79b5436ebb8b563f37a38f92a329b9e11f
Raphael Larrinaga <raphael.larrinaga@gmail.com>
2021-03-04 18:57:17 +0100

    Set src/dist structure and add v1 in the same branch for netlify

--- gulpfile.js
+++ gulpfile.js
@@ -21,9 +21,14 @@
   ];
 
   const paths = {
-    sass: './src/sass/**/*.scss',
-    js: './src/js/*.js',
-    html: './index.html',
+    styles: {
+      src: './src/sass/**/*.scss',
+      dist: './dist/assets/css'
+    },
+    html: {
+      src: './src/index.html',
+      dist: './dist/',
+    },
   };
 
   // Error notifications with notify.
@@ -34,11 +39,19 @@
     })(error);
   };
 
+  function copyV1() {
+    return gulp
+      .src('./src/v1/**/*')
+      .pipe(gulp.dest('./dist/v1'));
+  }
+
+  exports.copyV1 = copyV1;
+
   function compileHtml() {
     return gulp
-      .src(paths.html)
+      .src(paths.html.src)
       .pipe(htmlmin({collapseWhitespace: true}))
-      .pipe(gulp.dest('./'));
+      .pipe(gulp.dest(paths.html.dist));
   }
 
   exports.compileHtml = compileHtml;
@@ -47,7 +60,7 @@
   // Maybe see package.json npm scripts.
   function compileCSS() {
     return gulp
-      .src(paths.sass)
+      .src(paths.styles.src)
       .pipe(
         sass({
           outputStyle: 'compressed',
@@ -60,12 +73,12 @@
       )
       .pipe(postcss(processors))
       .pipe(plumber(reportError))
-      .pipe(gulp.dest('./assets/css'));
+      .pipe(gulp.dest(paths.styles.dist));
   }
 
   function watchCSS() {
     return gulp
-      .src(paths.sass)
+      .src(paths.styles.src)
       .pipe(
         sass({
           outputStyle: 'nested',
@@ -78,7 +91,7 @@
       )
       .pipe(postcss(processors))
       .pipe(plumber(reportError))
-      .pipe(gulp.dest('./assets/css'))
+      .pipe(gulp.dest(paths.styles.dist))
       .pipe(livereload());
   }
 
@@ -91,7 +104,10 @@
 
   exports.watch = watchFiles;
 
-  const build = gulp.series(compileHtml, compileCSS);
+  const build = gulp.series(compileHtml, compileCSS, copyV1);
   exports.build = build;
 
+  // Global task: $ gulp.
+  // Same as build. Has to be defined.
+  exports.default = build
 }());

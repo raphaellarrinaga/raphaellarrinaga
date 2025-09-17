719435ab2afcb297f42d7865d932df54026410a3
Raphael Larrinaga <raphael.larrinaga@gmail.com>
2025-09-17 10:57:49 +0200

    Set Gulp src and dist dirs. Update content.

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # Raphael Larrinaga Jardins
 
 HTML5 static page.
+Using Gulp 4. Don't Upgrade to unstable Gulp 5.
+
+## Gulp tasks
+
+- `gulp dev` : start server at `http://localhost:8080/` ready for livereload with chrome extension.
+- `gulp dist` : compile html & css (didn't set "build" to avoid "troubles" with netlify).
+
+## Updates
+
+- Versions are living on different branches
+- Master should be correctly tagged at branch merge
+
+## Roadmap
---
+++ gulpfile.js
@@ -0,0 +1,169 @@
+/**
+ * @file
+ */
+
+(function () {
+  // eslint-disable-next-line strict
+  'use strict';
+
+  const gulp = require('gulp');
+  // const sass = require('gulp-sass');
+  const sass = require('gulp-sass')(require('sass'));
+  const autoprefixer = require('autoprefixer');
+  const browserSync = require('browser-sync').create();
+  const postcss = require('gulp-postcss');
+  const htmlmin = require('gulp-htmlmin');
+  const notify = require('gulp-notify');
+  const plumber = require('gulp-plumber');
+  // const imagemin = require("gulp-imagemin");
+  const newer = require("gulp-newer");
+
+  const processors = [
+    autoprefixer(),
+  ];
+
+  const paths = {
+    styles: {
+      src: './src/assets/sass/**/*.scss',
+      dist: './dist/assets/css'
+    },
+    files: {
+      src: './src/assets/files/**/*',
+      dist: './dist/assets/files'
+    },
+    img: {
+    //   src: './src/assets/img/**/*',
+      src: './src/assets/img/**/*.{jpg,jpeg,png,svg,gif,webp}',
+      dist: './dist/assets/img'
+    },
+    html: {
+      src: './src/index.html',
+      dist: './dist/',
+    },
+  };
+
+  // Error notifications with notify.
+  const reportError = (error) => {
+    notify.onError({
+      title: 'Gulp error in ' + error.plugin,
+      message: error.toString()
+    })(error);
+  };
+
+  async function images() {
+    const imagemin = (await import("gulp-imagemin")).default;
+    const mozjpeg = (await import("imagemin-mozjpeg")).default;
+    const optipng = (await import("imagemin-optipng")).default;
+    const svgo = (await import("imagemin-svgo")).default;
+
+    // return gulp
+    //   .src(paths.img.src)
+    //   .pipe(gulp.dest(paths.img.dist));
+    return gulp
+      .src(paths.img.src, { buffer: true })
+      .pipe(newer(paths.img.dist))
+      .pipe(
+        imagemin([
+          mozjpeg({ quality: 75, progressive: true }),
+          optipng({ optimizationLevel: 5 }),
+          // svgo({
+          //   plugins: [
+          //     { name: "removeViewBox", active: false },
+          //     { name: "cleanupIDs", active: false },
+          //   ],
+          // }),
+        ], {
+          verbose: true,
+        })
+      )
+      .pipe(gulp.dest(paths.img.dist));
+  }
+
+  exports.images = images;
+
+  function files() {
+    return gulp
+      .src(paths.files.src)
+      .pipe(gulp.dest(paths.files.dist));
+  }
+
+  exports.files = files;
+
+  function compileHtml() {
+    return gulp
+      .src(paths.html.src)
+      .pipe(htmlmin({collapseWhitespace: true}))
+      .pipe(gulp.dest(paths.html.dist));
+  }
+
+  exports.compileHtml = compileHtml;
+
+  function compileCSS() {
+    return gulp
+      .src(paths.styles.src)
+      .pipe(
+        sass({
+          style: 'compressed',
+          sourceComments: false,
+          precision: 3,
+          // includePaths: [].concat(
+          //   'node_modules/normalize-scss/sass'
+          // ),
+        })
+      )
+      // .pipe(postcss(processors))
+      // .pipe(plumber(reportError))
+      .pipe(gulp.dest(paths.styles.dist));
+  }
+
+  function watchCSS() {
+    return gulp
+      .src(paths.styles.src)
+      .pipe(
+        sass({
+          outputStyle: 'nested',
+          sourceComments: true,
+          precision: 3,
+          includePaths: [].concat(
+            'node_modules/normalize-scss/sass'
+          ),
+        })
+      )
+      .pipe(postcss(processors))
+      .pipe(plumber(reportError))
+      .pipe(gulp.dest(paths.styles.dist))
+      // .pipe(browserSync.stream());
+      // .pipe(browserSync.stream());
+  }
+
+  exports.watchCSS = watchCSS;
+
+  function serve(done) {
+    browserSync.init({
+      server: {
+        baseDir: './dist',
+      },
+      ui: false,
+      open: false,
+      injectChanges: true,
+    });
+    done();
+  }
+
+  function watchFiles() {
+    gulp.watch(paths.html.src, compileHtml);
+    gulp.watch(paths.styles.src, watchCSS);
+  }
+
+  exports.watch = watchFiles;
+  const watch = watchFiles;
+  const dev = gulp.series(serve, watch);
+  exports.dev = dev;
+
+  const dist = gulp.series(compileHtml, compileCSS, images, files);
+  exports.dist = dist;
+
+  // Global task: $ gulp.
+  // Same as dist. Has to be defined.
+  exports.default = dist
+}());
